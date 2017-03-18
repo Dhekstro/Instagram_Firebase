3f3cb3613cad9da277575cd535dc4b8c68d0ab29
Dhekstro <claudiofs.paullo@gmail.com>
2017-03-18 12:20:51 +0000

    UIColor and Anchor Extension

---
+++ InstagramFirebase/Utils/FirebaseUtils.swift
@@ -0,0 +1,25 @@
+//
+//  FirebaseUtils.swift
+//  InstagramFirebase
+//
+//  Created by Cláudio Paulo on 4/12/17.
+//  Copyright © 2017 OmegaWare, Lda. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+extension Database {
+    
+    static func fetchUserWithUID(uid: String, completion: @escaping (User) -> ()) {
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            guard let userDictionary = snapshot.value as? [String: Any] else { return }
+            let user = User(uid: uid, dictionary: userDictionary)
+            completion(user)
+            
+        }) { (err) in
+            print("Failed to fetch user for posts:", err)
+        }
+    }
+}
