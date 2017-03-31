2f591d83302cba9a045c2bf645cb1400dfc9924f
Dhekstro <claudiofs.paullo@gmail.com>
2017-03-31 12:52:19 +0100

    Fetch Photos Framework

---
+++ InstagramFirebase/AddPhoto/PhotoSelectorHeader.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoSelectorHeader.swift
+//  InstagramFirebase
+//
+//  Created by Cláudio Paulo on 3/31/17.
+//  Copyright © 2017 OmegaWare, Lda. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorHeader: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .cyan
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
